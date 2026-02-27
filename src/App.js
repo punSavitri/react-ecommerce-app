d0154f065c052b4d18f18a2d5512f95d199d4800
SavitriPun <punshakshi@gmail.com>
2026-02-27 22:02:14 +0000

    create pages, define routes, replace Navbar li items with <Link>

--- src/App.js
+++ src/App.js
@@ -1,21 +1,48 @@
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Cart from "./pages/Cart";
 import LoginSignup from "./pages/LoginSignup";
 import Product from "./pages/Product";
 import Shop from "./pages/Shop";
 import ShopCategory from "./pages/ShopCategory";
+import men_banner from "./components/assets/banner_mens.png";
+import women_banner from "./components/assets/banner_women.png";
+import kids_banner from "./components/assets/banner_kids.png";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
     <div>
-      <Navbar />
-      {/* Shop page is a home page */}
-      <Shop />
-      <ShopCategory />
-      <Product />
-      <Cart />
-      <LoginSignup />
+      {/* setting routes */}
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          {/* Shop page is a home page */}
+          <Route path="/" element={<Shop />} />
+          <Route
+            path="/mens"
+            element={<ShopCategory category="men" banner={men_banner} />}
+          />
+          <Route
+            path="/womens"
+            element={<ShopCategory category="women" banner={women_banner} />}
+          />
+          <Route
+            path="/kids"
+            element={<ShopCategory category="kid" banner={kids_banner} />}
+          />
+          {/* setting route for product */}
+
+          <Route path="/product" element={<Product />}>
+            {/* setting route for product id path */}
+            <Route path=":productId" element={<Product />} />
+          </Route>
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<LoginSignup />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,11 +2,12 @@ import React, { useState } from "react";
 import "./Navbar.css";
 import logo from "../assets/logo.png";
 import cart_icon from "../assets/cart_icon.png";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   //define state variables for hamburger nav menu
   const [menuOpen, setMenuOpen] = useState(false);
-
+  const [menu, setMenu] = useState("shop");
 
   return (
     <>
@@ -15,21 +16,47 @@ const Navbar = () => {
           <img src={logo} alt="brand logo" />
           <p>HELLO🤍SHOPPER</p>
         </div>
-        
+
         {/* Menu */}
         <ul className={menuOpen ? "nav-menu active" : "nav-menu"}>
-          <li>Shop</li>
-          <li>Men</li>
-          <li>Women</li>
-          <li>Kids</li>
+          <li onClick={() => setMenu("shop")}>
+            <Link style={{ textDecoration: "none" }} to="/">
+              Shop
+            </Link>
+            {menu === "shop" ? <hr /> : <></>}
+          </li>
+          <li onClick={() => setMenu("mens")}>
+            <Link style={{ textDecoration: "none" }} to="/mens">
+              Men
+            </Link>
+            {menu === "mens" ? <hr /> : <></>}
+          </li>
+          <li onClick={() => setMenu("womens")}>
+            <Link style={{ textDecoration: "none" }} to="/womens">
+              Women
+            </Link>
+            {menu === "womens" ? <hr /> : <></>}
+          </li>
+          <li onClick={() => setMenu("kids")}>
+            <Link style={{ textDecoration: "none" }} to="/kids">
+              Kids
+            </Link>
+            {menu === "kids" ? <hr /> : <></>}
+          </li>
         </ul>
         <div className="nav-login-cart">
-          <button>Login</button>
-          <img src={cart_icon} alt="img of shopping cart" />
+          <Link to="/login">
+            <button>Login</button>
+          </Link>
+          <Link to="/cart">
+            {" "}
+            <img src={cart_icon} alt="img of shopping cart" />
+          </Link>
+
           <div className="nav-cart-count">0</div>
         </div>
         {/* Hamburger Icon */}
-        <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+        <div className="hamburger-menu" onClick={() => setMenuOpen(!menuOpen)}>
           <span></span>
           <span></span>
           <span></span>
