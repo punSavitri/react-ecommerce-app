dc4038e3f2b113c7bd7021c42abb06186a24dbe9
SavitriPun <punshakshi@gmail.com>
2026-02-28 16:06:10 +0000

    created & styled NewsLetter component as subscription box, mount to Shop page

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -3,14 +3,16 @@ import Hero from "../components/hero/Hero";
 import Popular from "../components/popular/Popular";
 import Offers from "../components/offers/Offers";
 import NewCollections from "../components/newcollections/NewCollections";
+import NewsLetter from "../components/newsletter/NewsLetter";
 
 const Shop = () => {
   return (
     <div>
       <Hero />
       <Popular />
-      <Offers/>
-      <NewCollections/>
+      <Offers />
+      <NewCollections />
+      <NewsLetter />
     </div>
   );
 };
