00319f9c5ded5a729e8b9e2e0a46b11568f35b94
SavitriPun <punshakshi@gmail.com>
2026-03-03 23:08:23 +0000

    created layout html structures of LoginSignup component

--- src/pages/LoginSignup.jsx
+++ src/pages/LoginSignup.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
 
 const LoginSignup = () => {
   return (
-    <div>LoginSignup</div>
-  )
-}
+    <div className="loginsignup">
+      <div className="loginsignup-container">
+        <h1>Sign Up</h1>
+        <div className="loginsignup-fields">
+          <input type="text" placeholder="enter your name" />
+          <input type="email" placeholder="your email address" />
+          <input type="password" placeholder="password" />
+        </div>
+        <button>Continue</button>
+        <p className="loginsignup-login">
+          Already have an account? <span>Login Here </span>
+        </p>
+        <div className="loginsignup-agree">
+          <input type="checkbox" name="" id="" />
+          <p>By continuing, I agree to the terms of use & privacy policy </p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default LoginSignup
+export default LoginSignup;
