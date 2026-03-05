8fe55da7ef97b4abb292daa8be787d0e3c7a820d
SavitriPun <punshakshi@gmail.com>
2026-03-05 22:21:17 +0000

    created ProductDisplay component to show product images and description

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { useParams } from "react-router-dom";
 import { ShopContext } from "../context/ShopContext";
 import Breadcrumb from "../components/breadcrumbs/Breadcrumb";
+import ProductDisplay from "../components/productDisplay/ProductDisplay";
 
 const Product = () => {
   const { all_product } = useContext(ShopContext);
@@ -10,6 +11,7 @@ const Product = () => {
   return (
     <div>
       <Breadcrumb product={product} />
+      <ProductDisplay product={product} />
     </div>
   );
 };
