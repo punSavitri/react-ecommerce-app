9d90d1c6635ce6d2b842dabbf7b295c6f44f3fd3
SavitriPun <punshakshi@gmail.com>
2026-03-10 00:02:49 +0000

    added style to CartItems component

--- src/components/cartitems/CartItems.jsx
+++ src/components/cartitems/CartItems.jsx
@@ -22,15 +22,16 @@ const CartItems = () => {
         if (cartItems[e.id] > 0) {
           return (
             <div>
-              <div className="cartitems-format">
+              <div className="cartitems-format cartitems-format-main">
                 <img src={e.image} alt="" className="cartitems-product-icon" />
                 <p>{e.name}</p>
                 <p>${e.new_price}</p>
                 <button className="cartitems-quantity">
                   {cartItems[e.id]}
                 </button>
-                <p>{e.new_price * cartItems[e.id]}</p>
+                <p>${e.new_price * cartItems[e.id]}</p>
                 <img
+                className="cartitems-remove-icon"
                   src={remove_icon}
                   alt=""
                   onClick={() => {
@@ -38,10 +39,41 @@ const CartItems = () => {
                   }}
                 />
               </div>
+              <hr />
             </div>
           );
         }
+        return null;
       })}
+      <div className="cartitems-down">
+        <div className="cartitems-total">
+          <h1>Cart Totals</h1>
+          <div>
+            <div className="cartitems-total-item">
+              <p>Subtotal</p>
+              <p>${0}</p>
+            </div>
+            <hr />
+            <div className="cartitem-total-item">
+              <p>Shipping Fee</p>
+              <p>Free</p>
+            </div>
+            <hr />
+            <div className="cartitem-total-item">
+              <h3>Total</h3>
+              <h3>${0}</h3>
+            </div>
+          </div>
+          <button>Proceed to Chekout</button>
+        </div>
+        <div className="cartitems-promocode">
+          <p>If you have a promo code, please enter it here</p>
+          <div className="cartitems-promobox">
+            <input type="text" placeholder="promo code" />
+            <button>Submit</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
